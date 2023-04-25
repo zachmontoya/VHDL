61b6bc0b05fcbb784af92747cf29c283a4808127
zachmontoya <95238910+zachmontoya@users.noreply.github.com>
2023-04-24 23:16:55 -0600

    Lab2

---
+++ Lab2/Lab2PartA.vhd
@@ -0,0 +1,29 @@
+-- Lab #2: Behavioral-to-Schematic Translation, Index Statement 
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Index is
+    Port (
+        ctrl: in std_logic_vector(1 downto 0);
+        a: out std_logic_vector(3 downto 0)
+    );
+end Index;
+
+architecture rtl of Index is
+
+signal au: unsigned(3 downto 0);
+begin
+
+    process (au, ctrl)
+    begin
+    au <= (others=>'0');
+    
+    au(to_integer(unsigned(ctrl))) <= '1';
+    
+    a <= std_logic_vector(au);
+    
+    end process;
+
+end rtl;
---
+++ Lab2/Lab2PartBCond.vhd
@@ -0,0 +1,37 @@
+-- Lab #2: Behavioral-to-Schematic Translation, Selected and Conditional Signal Statements
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ZachsEntityName is
+    port (
+        condselector : in std_logic_vector(2 downto 0);
+        Ainput : in std_logic;
+        Binput : in std_logic;
+        Cinput : in std_logic;
+        MainOutput1 : out std_logic;
+        MainOutput2 : out std_logic
+        );
+end ZachsEntityName;
+
+architecture ZachsLogic of ZachsEntityName is
+begin
+    --Golden Rule 3 
+    MainOutput1 <= 0
+    MainOutput2 <= 0
+
+    -- Selected Signal Statement
+    MainOutput1 <= Ainput when (condselector = "000") else
+                   Binput when (condselector = "010") else     
+                   Cinput;
+    
+    
+    -- Conditional Signal Statement
+    with condselector select
+    MainOutput2 <= Ainput when "000",
+                   Binput when "010",
+                   Cinput when others;
+
+end architecture ZachsLogic;
+
+
+
